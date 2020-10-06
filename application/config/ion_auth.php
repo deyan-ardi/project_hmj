9cdde9b1c8353666655a19c14f4b775e3e094aef
Deyan Ardi <riyan.clsg11@gmail.com>
2020-10-06 22:56:24 +0800

    eors siap di up

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -134,7 +134,7 @@ $config['argon2_admin_params']		= [
  */
 $config['site_title']                 = "SSO Informatic";       // Site Title, example.com
 $config['admin_email']                = "hmjtiundiksha@gmail.com"; // Admin Email, admin@example.com
-$config['default_group']              = 'members';           // Default group, use name
+$config['default_group']              = 'inti_hmj';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             /* You can use any unique column in your table as identity column.
 															    The values in this column, alongside password, will be used for login purposes
